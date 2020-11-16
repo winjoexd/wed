b218410058f7c3f7078e686b3c35b302f2835635
winjoexd <winjoexd@gmail.com>
2020-11-16 09:47:06 +0800

    add WEditor string

--- wed.c
+++ wed.c
@@ -335,7 +335,7 @@ void editorDrawStatusBar(struct abuf *ab)
 	abAppend(ab, "\x1b[7m", 4);
 
 	len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
-	rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+	rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d [WEDitor]", E.cy + 1, E.numrows);
 	if (len > E.screencols) len = E.screencols;
 	abAppend(ab, status, len);
 
